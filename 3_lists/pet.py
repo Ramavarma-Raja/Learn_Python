3e8c7e737500224d15e37b82b1aaaf542d22134a
Ramavarma-Raja <ramavraja@gmail.com>
2026-03-31 06:31:10 +0530

    31st Mar 2026

---
+++ 3_lists/pet.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'fish', 'parrot']
+
+for animal in animals:
+    print(f"{animal.title()} would make a good pet.")
+
+print("\nAny of the above animals can make a good pet!")
---
+++ 3_lists/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['margherita', 'pepperoni', 'hawaiian']
+
+for pizza in pizzas:
+    print(f"I like {pizza.title()}.")
+
+print("\nI love Pizzas!")
